0b906653d0ba3cb256f77aa0d4de991ffd98d33c
nephna <ilunamist@gmail.com>
2016-04-18 14:31:47 -0400

    Fixed some bugs

--- server.go
+++ server.go
@@ -8,7 +8,6 @@ import (
 	"io/ioutil"
 	"time"
 	"net/http"
-	"net/url"
 	"html/template"
 )
 
@@ -17,7 +16,6 @@ const (
 	MAIN = "/main"
 	UPLOAD = "/upload"
 	LOGOUT = "/logout"
-	DOWNLOAD = "/download"
 	SLASH = "/"
 	BLANK = ""
 
@@ -57,14 +55,14 @@ func startSession (writer http.ResponseWriter, username string) {
 
 // refreshes a currently active session or redirects to login page if the
 // session has expired
-func refreshSession (writer http.ResponseWriter, request *http.Request) {
+func refreshSession (writer http.ResponseWriter, request *http.Request) (bool) {
 	if (validSession(request)) {
 		name := getName(request)
 		startSession(writer, name)
-		return
+		return true
 	}
 	http.Redirect(writer, request, LOGIN, 301)
-	return
+	return false
 }
 
 // ends the session by clearing the cookie
@@ -92,12 +90,19 @@ func validSession (request *http.Request) (bool) {
 // returns a name from the session cookie
 func getName (request *http.Request) (username string) {
 	cookie, err := request.Cookie("username")
-	checkError(err)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+	}
 	return cookie.Value
 }
 
 // parses textbox input, validates it, and if valid, starts a new session for the user
 func login (writer http.ResponseWriter, request *http.Request) {
+	if (validSession(request)) {
+		http.Redirect(writer, request, MAIN, 301)
+		return
+	}
+
 	if (request.Method == "GET") {
 		t, _ := template.ParseFiles(LOGIN_HTML)
 		t.Execute(writer, nil)
@@ -116,14 +121,17 @@ func login (writer http.ResponseWriter, request *http.Request) {
 }
 
 func internal (writer http.ResponseWriter, request *http.Request) {
-	refreshSession(writer, request)
+	if (!refreshSession(writer, request)) {
+		return
+	}
 
 	if (request.Method == "GET") {
 		dir := FILE_LOCATION + getName(request) + string(os.PathSeparator)
 		files, _ := ioutil.ReadDir(dir)
 		codeBody := ""
 		for _, file := range files {
-			codeBody += "<tr><td><a href=\"" + file.Name() + "?download\">" + file.Name() + "</a></td></tr>\n"
+			codeBody += "<tr><td><a href=\"" + file.Name() + "?download\">" + file.Name() + "</a></td>" +
+							"<td><a href=\"" + file.Name() + "?remove\">Remove</a></td></tr>\n"
 		}
 
 		t, _ := template.ParseFiles(MAIN_HTML)
@@ -136,8 +144,7 @@ func internal (writer http.ResponseWriter, request *http.Request) {
 			getName(request), // get name
 			template.HTML(codeBody), // convert string to html code
 		}
-		err := t.Execute(writer, inject)
-		checkError(err)
+		t.Execute(writer, inject)
 	}
 }
 
@@ -149,7 +156,9 @@ func logout (writer http.ResponseWriter, request *http.Request) {
 
 
 func upload (writer http.ResponseWriter, request *http.Request) {
- 	refreshSession(writer, request)
+ 	if (!refreshSession(writer, request)) {
+ 		return
+ 	}
 
  	if (request.Method == "GET") {
  		t, _ := template.ParseFiles(UPLOAD_HTML)
@@ -160,6 +169,7 @@ func upload (writer http.ResponseWriter, request *http.Request) {
 	 	if (err != nil) {
 	 		// file not found, try again
 	 		http.Redirect(writer, request, UPLOAD, 301)
+	 		return
 	 	}
 	 	defer file.Close()
 
@@ -169,48 +179,81 @@ func upload (writer http.ResponseWriter, request *http.Request) {
 	 	// TODO: error checking and handling
 	 	dir := FILE_LOCATION + name + string(os.PathSeparator)
 	 	err = os.MkdirAll(dir, 0766) // makes a new directory for this user
+	 	if (err != nil) {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+			return
+		}
 
 	 	destName := dir + header.Filename
 
 	 	output, err := os.Create(destName)
-	 	checkError(err)
+	 	if (err != nil) {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+			return
+		}
 	 	defer output.Close()
 
 	 	// write the content from POST to the file
 	 	_, err = io.Copy(output, file)
-	 	checkError(err)
+	 	if (err != nil) {
+			fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+		}
 
 	 	// file uploaded successfully, redirect to main page
 	 	http.Redirect(writer, request, MAIN, 301)
 	 }
  }
 
+ func downloadOrRemove (writer http.ResponseWriter, request *http.Request) {
+ 	if (!refreshSession(writer, request)) {
+ 		return
+ 	}
+
+ 	query := request.URL.Query()
+ 	fmt.Println(query)
+
+ 	if (len(query["remove"]) > 0) {
+ 		remove(writer, request)
+ 	} else if (len(query["download"]) > 0) {
+ 		download(writer, request)
+ 	}
+ }
+
  func download (writer http.ResponseWriter, request *http.Request) {
- 	refreshSession(writer, request)
+	location := FILE_LOCATION + getName(request) + request.URL.Path
+	fmt.Println(location)
+	file, err := os.Open(location)	
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+		http.Redirect(writer, request, MAIN, 301)
+		return
+	}
+	defer file.Close()
 
-	query, err := url.ParseQuery(request.URL.RawQuery)
-	checkError(err)
+	writer.Header().Set("Content-Type", "application/octet-stream") // set to download file
 
-	if (len(query["download"]) > 0) {
-		writer.Header().Set("Content-Type", "application/octet-stream") // set to download file
+	array := make([]byte, 32000) // new buffer of size 32k bytes
+	for (true) {
+		n, err := file.Read(array) // read from file
+		writer.Write(array[:n]) // write to client
 
-		location := FILE_LOCATION + getName(request) + request.URL.Path
-		file, err := os.Open(location)
-		defer file.Close()
-		checkError(err)
-
-		array := make([]byte, 32000) // new buffer of size 32k bytes
-		for (true) {
-			n, err := file.Read(array) // read from file
-			writer.Write(array[:n]) // write to client
-
-			if (err == io.EOF) {
-				break
-			} else {
-				checkError(err)
+		if (err != nil) {
+			if (err != io.EOF) {
+				fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
 			}
+			break
 		}
 	}
+ }
+
+ func remove (writer http.ResponseWriter, request *http.Request) {
+	location := FILE_LOCATION + getName(request) + request.URL.Path
+	fmt.Println(location)
+	err := os.Remove(location)
+	if (err != nil) {
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
+	}
+	http.Redirect(writer, request, MAIN, 301)
  }
 
 func main () {
@@ -224,17 +267,13 @@ func main () {
 	http.HandleFunc(MAIN, internal)
 	http.HandleFunc(LOGOUT, logout)
 	http.HandleFunc(UPLOAD, upload)
-	http.HandleFunc(SLASH, download)
+	http.HandleFunc(SLASH, downloadOrRemove)
 
 	port := ":" + os.Args[1] // get port from user
 
 	err := http.ListenAndServe(port, nil) // begin listening on port
-	checkError(err)
-}
-
-func checkError (err error) {
 	if (err != nil) {
-		fmt.Fprintf(os.Stderr, "Fatal error: %s\n", err.Error())
+		fmt.Fprintf(os.Stderr, "Error: %s\n", err.Error())
 		os.Exit(1)
 	}
 }
